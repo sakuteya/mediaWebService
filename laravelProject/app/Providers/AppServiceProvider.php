53b5c5b18970a07cc13adbac677a16bfa3840076
sakuteya <deliakka.gebageba@gmail.com>
2018-08-17 23:29:51 +0900

    fix:マイグレーションファイル実行エラー対応
    ユニーク制約付きvarcharは767byteまでのため、varcharのデフォルト値を191へ制限。
    参考：https://teratail.com/questions/63441

--- laravelProject/app/Providers/AppServiceProvider.php
+++ laravelProject/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //varcharの767byte制限
+        Schema::defaultStringLength(191);
     }
 
     /**
