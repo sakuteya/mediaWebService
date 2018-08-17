2f73acc9ee1dc8039fb8a71900fb827ab765d9dc
sakuteya <deliakka.gebageba@gmail.com>
2018-08-17 23:41:03 +0900

    [add]サンプルページ作成
    hello worldとVCのサンプル作成。

---
+++ laravelProject/resources/views/testSak.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testesSak</title>
+</head>
+<body>
+    <p>hello world!!</p>
+    <p>{{$data['name']}}さん</p>
+    <p>{{$data['message']}}</p>
+    <p>現在は {{$today}} です</p>
+</body>
+</html>
--- laravelProject/routes/web.php
+++ laravelProject/routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('testSak', 'testSakController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
