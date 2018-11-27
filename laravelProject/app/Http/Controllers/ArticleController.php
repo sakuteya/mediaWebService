090223898da21f2682b0c826842b277ac2f23f65
sakuteya <deliakka.gebageba@gmail.com>
2018-11-28 08:28:55 +0900

    [add]Userモデル作成
    namespaceのディレクトリを明示することで同名クラスをmake可能。
    > php artisan make:model Models\User

--- laravelProject/app/Http/Controllers/ArticleController.php
+++ laravelProject/app/Http/Controllers/ArticleController.php
@@ -5,20 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ArticleController extends Controller
 {
     
     public function index($userName, $title){
 
-        // ユーザーが存在すれば、idを設定。
-        $userId = '';
-        if(DB::table('users')->where('name', $userName)->exists()) {
-            $userId = DB::table('users')->where('name', $userName)->first()->id;
-        }
         //対象記事がなければ404を返す。
+        $userId = User::where('name', $userName)->firstOrFail()->id;
         $articel = Article::where('user_id', $userId)->where('title', $title)->firstOrFail();
         $vTitle = $articel->title;
         $vBody = $articel->body;
