db144d195a7b3d137a9a8034d2670f9554302bc1
sakuteya <deliakka.gebageba@gmail.com>
2018-11-30 16:46:15 +0900

    [add]記事に対応するタグを表示する
    見た目は１記事を囲んでデザインした

--- laravelProject/database/factories/TagFactory.php
+++ laravelProject/database/factories/TagFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Tag::class, function (Faker $faker) {
     return [
         //
-        'tag' => $faker->name,
+        'tag' => $faker->word,
     ];
 });
--- laravelProject/resources/views/user.blade.php
+++ laravelProject/resources/views/user.blade.php
@@ -3,19 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header">{{$vUserName}}</div>
-
                 <div class="card-body">
                     <p>{{$vProfile}}</p>
                 </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">記事一覧</div>
                 <div class="card-body">
-
                     @forelse ($vArticels as $articel)
-                        <div>
-                            {{-- TODO: 記事リンクをbladeでごちゃごちゃ作りたくない --}}
-                            <a href='{{ url("/article/{$vUserName}/{$articel->title}") }}'>{{ $articel->title }}</a>
+                        <div class="border-bottom p-3">
+                            <div>
+                                {{-- TODO: 記事リンクをbladeでごちゃごちゃ作りたくない --}}
+                                <a href='{{ url("/article/{$vUserName}/{$articel->title}") }}'>{{ $articel->title }}</a>
+                            </div>
+                            <div class="d-flex">
+                                @foreach ($articel->tags as $tag)
+                                    <a href="#" class="badge badge-dark mr-1">{{$tag->tag}}</a>
+                                @endforeach
+                            </div>
                         </div>
                     @empty
                         <p>記事なし</p>
