51f1d32bc48528f4e4d0bf73f092f494cb17b1a3
sakuteya <deliakka.gebageba@gmail.com>
2018-12-06 19:58:06 +0900

    .gitignore is now working

--- laravelProject/resources/views/listArticles.blade.php
+++ laravelProject/resources/views/listArticles.blade.php
@@ -12,13 +12,19 @@
                     </nav>
                     @forelse ($vArticels as $articel)
                         <div class="border-bottom p-3">
+                            <div class="d-flex small text-secondary">
+                                <span>投稿者:</span>
+                                <a href="#" class="mr-1">{{ $articel->user->name }}</a>
+                                <span>更新日時:</span>
+                                <span>{{$articel->updated_at}}</span>
+                            </div>
                             <div>
                                 {{-- TODO: 記事リンクをbladeでごちゃごちゃ作りたくない --}}
-                                <a href='{{ url("/article/{$articel->user->name}/{$articel->title}") }}'>{{ $articel->title }}</a>
+                                <a href='{{ url("/article/{$articel->user->name}/{$articel->title}") }}' class="h3">{{ $articel->title }}</a>
                             </div>
                             <div class="d-flex">
                                 @foreach ($articel->tags as $tag)
-                                    <a href="#" class="badge badge-dark mr-1">{{$tag->tag_name}}</a>
+                                    <a href="#" class="badge badge-secondary mr-1">{{$tag->tag_name}}</a>
                                 @endforeach
                             </div>
                         </div>
